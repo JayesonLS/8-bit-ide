8f6ef58c39db072811bdd27c5e5c447ac7a84c39
Jayeson Lee-Steere <jayeson@titaniumstudios.com>
2021-10-12 17:30:50 +0800

    Initial work on PicoFirmware experiments. Just a hello-world shell at the moment.

---
+++ PicoFirmware/8bit_ide_drive.cpp
@@ -0,0 +1,36 @@
+// Copyright (C) 2021 Titanium Studios Pty Ltd 
+// 
+// This program is free software : you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.If not, see < https://www.gnu.org/licenses/>.
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main()
+{
+    stdio_init_all();
+
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    for (int value = 1; ; value ^= 1)
+    {
+        printf("8bit_ide_drive heartbeat.\n");
+
+        gpio_put(LED_PIN, value);
+
+        sleep_ms(1000);
+    }
+    return 0;
+}
