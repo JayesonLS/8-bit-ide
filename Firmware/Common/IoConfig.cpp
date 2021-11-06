c0b893bb61bf2e6055c2e28765a68d1107981549
Jayeson Lee-Steere <jayeson@titaniumstudios.com>
2021-11-06 11:14:19 +0800

    Complete PreInitialize() method.

--- Firmware/Common/IoConfig.cpp
+++ Firmware/Common/IoConfig.cpp
@@ -18,6 +18,15 @@
 
 /*static*/ void IoConfig::PreInitialzieIo()
 {
+    // We don't call gpio_init() for two reasons. First
+    // is that it will be a little slower as it only does
+    // one pin at a time. Second is that it will switch
+    // everything to inputs first while we want some to 
+    // be outputs. If we are doing some kind of reset and
+    // the registers have not been set to default values,
+    // then better to assume some pins might already be in
+    // the state that we want.
+
     // The order here is chosen on purpose.
     // First, disable any outputs that should not be inputs,
     // then clear/set in/out values as needed, then enable
@@ -28,6 +37,10 @@
     sio_hw->gpio_oe_set = INITIAL_OUTPUTS;
 
     // To start, configure any pins we are using as SIO's.
+    // gpio_set_function enables inputs/outputs to function 
+    // It also clears all of the GPIOx_CONTROL register
+    // (signal inversions, etc.) except for setting the
+    // function to SIO. 
     for (uint pinIndex = 0, mask = INITIAL_INPUTS_AND_OUTPUTS; pinIndex < 32; pinIndex++, mask >>= 1)
     {
         if (mask & 0x01)
@@ -35,18 +48,4 @@
             gpio_set_function(pinIndex, GPIO_FUNC_SIO);
         }
     }
-/*    gpio_set_function()
-
-     GPIO_FUNC_XIP = 0,
-    GPIO_FUNC_SPI = 1,
-    GPIO_FUNC_UART = 2,
-    GPIO_FUNC_I2C = 3,
-    GPIO_FUNC_PWM = 4,
-    GPIO_FUNC_SIO = 5,
-    GPIO_FUNC_PIO0 = 6,
-    GPIO_FUNC_PIO1 = 7,
-    GPIO_FUNC_GPCK = 8,
-    GPIO_FUNC_USB = 9,
-    GPIO_FUNC_NULL = 0x1f,   
-*/
 }
