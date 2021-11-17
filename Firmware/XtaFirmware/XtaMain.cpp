32c16e4d12cd0f86e659b8151fae7b12365ef2cd
Jayeson Lee-Steere <jayeson@titaniumstudios.com>
2021-11-17 18:00:34 +0800

    A bunch of second core polling loop experiments. Finally figured out how to include a function in scratch_x and call it from C++.

--- Firmware/XtaFirmware/XtaMain.cpp
+++ Firmware/XtaFirmware/XtaMain.cpp
@@ -50,50 +50,153 @@ static void RunTests()
 }
 
 
+#include "hardware/regs/pio.h"
+#include "hardware/regs/addressmap.h"
+
+
 #define __scratch_x_func(func_name) __scratch_x(__STRING(func_name)) func_name
 
 extern "C"
 {
     volatile uint __scratch_x_func(loopCount);
     extern void SecondCorePollingLoop();
+    extern int SimpleFunc();
+
+    __attribute__ ((noinline)) int __scratch_x_func(SimpleRef)()
+    {
+        return 1;
+    }
+
 
     void __scratch_x_func(SecondCorePollingLoopCRef)()
     {
+        static const uint32_t sm = 0;
         uint count = 0;
+        uint32_t regValue;
 
         // Just a mock up of what the tight polling loop needs to do.
 
         while(1)
         {
-    //        if (pio0->intr)
-    //        {
-    //            pio0->intr = 0;
-    //        }
-    //
-    //        if (pio1->intr)
-    //        {
-    //            pio1->intr = 0;
-    //        }
-    //
-    //        if (!pio_sm_is_rx_fifo_empty(pio0, 0))
-    //        {
-    //            pio_sm_get(pio0, 0);
-    //        }
-    //
-    //        if (sio_hw->fifo_st & SIO_FIFO_ST_VLD_BITS)
-    //        {
-    //            (void) sio_hw->fifo_rd;
-    //        }
-
             count++;
             loopCount = count;
+
+            regValue = pio0->intr;
+            if (regValue & (PIO_INTR_SM0_BITS | PIO_INTR_SM1_BITS))
+            {
+                goto pio0intr;
+            }
+            for (int i = 0 ; i < 1000; i++ )
+            {
+                
+            }
+        
+            regValue = pio1->intr;
+            if (regValue & (PIO_INTR_SM0_BITS))
+            {
+                goto pio1intr;
+            }
+
+            for (int i = 0 ; i < 1000; i++ )
+            {
+
+            }
+    
+            regValue = pio0->fstat;
+            if (regValue &  (1u << (PIO_FSTAT_RXEMPTY_LSB + sm)))
+            {
+                goto pio0sm0fifo;
+            }
+
+            for (int i = 0 ; i < 1000; i++ )
+            {
+                
+            }
+
+            regValue = sio_hw->fifo_st;
+            if (!(regValue & SIO_FIFO_ST_VLD_BITS))
+            {
+                continue;
+            }
+            switch (sio_hw->fifo_rd & 0x07)
+            {
+                case 0:
+                    for (int i = 0 ; i < 100; i++ )
+                    {
+                        (void)sio_hw->fifo_rd;
+                    }
+                    break;
+                case 1:
+                    for (int i = 0 ; i < 1000; i++ )
+                    {
+                        (void)sio_hw->fifo_rd;
+                    }
+                    break;
+                case 2:
+                    for (int i = 0 ; i < 10000; i++ )
+                    {
+                        (void)sio_hw->fifo_rd;
+
+                    }
+                    break;
+                case 3:
+                    for (int i = 0 ; i < 100000; i++ )
+                    {
+                        (void)sio_hw->fifo_rd;
+                        
+                    }
+                    break;
+                case 4:
+                    for (int i = 0 ; i < 1000000; i++ )
+                    {
+                        (void)sio_hw->fifo_rd;
+                        
+                    }
+                    break;
+                case 5:
+                    for (int i = 0 ; i < 200; i++ )
+                    {
+                        (void)sio_hw->fifo_rd;
+                        
+                    }
+                    break;
+                case 6:
+                    for (int i = 0 ; i < 300; i++ )
+                    {
+                        (void)sio_hw->fifo_rd;
+                        
+                    }
+                    break;
+                case 7:
+                    for (int i = 0 ; i < 100; i++ )
+                    {
+                        (void)sio_hw->fifo_rd;
+                        
+                    }
+                    break;
+            }
+            continue;
+
+        pio0intr:
+            pio0->intr = 0;
+            continue;
+
+        pio1intr:
+            pio1->intr = 0;
+            continue;
+
+        pio0sm0fifo:
+            pio_sm_get(pio0, 0);
+            continue;
+
+
         }
     }
 }
 
 void SecondCorePollingLoopOuter()
 {
-    SecondCorePollingLoop();
+    SecondCorePollingLoopCRef();
 }
 
 
@@ -114,8 +217,14 @@ int main()
 
         printf("SecondCorePollingLoop() address: %08X\n", (uint)SecondCorePollingLoop);
 
-    	multicore_launch_core1(SecondCorePollingLoopOuter);
+        int value = SimpleFunc();
+
+        printf("Still alive %d\n", value);
+
+//    	multicore_launch_core1(SecondCorePollingLoopOuter);
     	//multicore_launch_core1(SecondCorePollingLoopCRef);
+
+
 
         while(1)
         {
