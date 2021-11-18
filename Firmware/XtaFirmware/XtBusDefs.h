e162e918fa49218a65b127ac2e5998844c19d486
Jayeson Lee-Steere <jayeson@titaniumstudios.com>
2021-11-18 19:28:52 +0800

    Shell for XtBus class.

---
+++ Firmware/XtaFirmware/XtBusDefs.h
@@ -0,0 +1,28 @@
+// Copyright (C) 2021 Titanium Studios Pty Ltd 
+// 
+// This program is free software : you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.If not, see < https://www.gnu.org/licenses/>.
+
+#ifndef XT_BUS_DEFS_H
+#define XT_BUS_DEFS_H
+
+#include "hardware/regs/addressmap.h"
+#include "hardware/regs/sio.h"
+#include "hardware/regs/pio.h"
+
+#define WRITE_PIO_BASE PIO0_BASE
+#define READ_PIO_BASE PIO1_BASE
+
+#define WRITE_CONTROL_REGISTER_SM 0
+
+#endif // XT_BUS_DEFS_H
